f748dc3a778cd7e0a82837e132c857ddc67cba73
RirikiNakabayashi <ririki-nakabayashi@united.jp>
2018-11-13 11:05:25 +0900

    DBのコンソール表示完了 & お掃除

--- src/main/java/com/example/sample/controller/SampleController.java
+++ src/main/java/com/example/sample/controller/SampleController.java
@@ -1,74 +1,19 @@
-/**
-game, manage, operation, salesのみ（account, clothes, event, guild, historyは重いため入れていない）
-
-以下の内容を抽出する
-【DB】
-game
-
-【テーブル】
-m_battery
-
-【カラム】
-battery_id
-sustained_time
-sheep_birth_time
-appearance_stable_id
-use_point
-del_flg
-start_date
-end_date
-create_date
-update_date
-
-
-【注意】
-Daoを呼び出すときは、Daoファイルに以下の記述が必要
-import org.seasar.doma.boot.ConfigAutowireable;
-@ConfigAutowireable
-*/
-
 package com.example.sample.controller;
 
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.ui.Model;
-//import com.example.sample.entity.game.MBattery;
-//import com.example.sample.repository.game.MBatteryDao;
 import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+
 	public class SampleController {
+	
 		@RequestMapping("/")
 		String home() {
 			return "home";
 		}
-		
-//		@Autowired
-//			private MBatteryDao Optional<MBatteryDao>;
-//		
 		@RequestMapping("/sample")
-//		public MBattery main(Model model) {
-//			model.addAttribute("message", "Hello Thymeleaf!!");
-//			MBattery re = null;
-//			model.addAttribute("batteryId", re.getBatteryId());
-//			return re;
 		String sample() {
 			return "sample";
 		}
+		
 	}
-
-
-//@Autowired
-//private MBatteryDao MBatteryDao;
-//
-//@RequestMapping(value = "/sample", method = RequestMethod.GET)
-//public MBattery main(Model model) {
-//Integer batteryId = 1;
-//	MBattery re = MBatteryDao.selectById(batteryId);
-//	model.addAttribute("batteryId", re.getBatteryId());
-//		return re;
-//	}
-//
-//}
--- src/main/java/com/example/sample/controller/SampleDataController.java
+++ src/main/java/com/example/sample/controller/SampleDataController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
 import com.example.sample.entity.sales.MGachaCountry;
 import com.example.sample.repository.sales.MGachaCountryDao;
+import com.example.sample.entity.sales.MGacha;
+import com.example.sample.repository.sales.MGachaDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,12 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 		
 		@Autowired
 		private MGachaCountryDao MGachaCountryDao;
-
-		@RequestMapping(value = "/sample/data", method = RequestMethod.GET)
-		public Optional<MGachaCountry> main() {
-		Integer gachaId = 10186;
-		Integer countryCd = 1;
-		Optional<MGachaCountry> re = MGachaCountryDao.selectById(gachaId, countryCd);
-				return re;
+		
+		@RequestMapping(value = "/sampleData/data", method = RequestMethod.POST)
+		public Optional<MGachaCountry> data(Integer gachaId, Integer countryCd) {
+			Optional<MGachaCountry> re = MGachaCountryDao.selectById(gachaId, countryCd);
+					return re;
 		}
+		
 	}
